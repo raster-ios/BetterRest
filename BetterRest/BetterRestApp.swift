b87e33d1c4d039386587dd92131896dfdf97b48a
Gennady Raster <raster.iosdev@gmail.com>
2021-12-13 15:55:40 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gena Raster on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
